df40eeb1a6a152584ef8f2eac544969c8b3f0246
MaksRus777 <mizyuk.maks@mail.ru>
2025-09-30 15:21:15 +0300

    web1lesson2

--- dj_prg/dj_app/views.py
+++ dj_prg/dj_app/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+
+#
+
+# главная
+def index(request):
+    return HttpResponse("<h2>Привет</h2>")  # Способ 1 записать внутрь HTML код
+    pass
+# о себе
+def about(request):
+    return HttpResponseRedirect("/")  # редирект на главную страницу
+
+def contact(request): # 3 способ с использованием HTML аблона
+    name ='Вася'
+    number = '37529999999'
+    context = {'NUMBER': number, 'NAME': name}# Способ для передачи данных из питон в HTMl
+    return render(request, "contact.html", context=context)
+
+
